ba309c48d548f62f0102d786620aaca1820a1989
hamid3potter <hrm.shams@gmail.com>
2017-09-01 14:52:20 +0430

    first commit with some improvement!

---
+++ UsersManagement/Database.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+
+namespace UsersManagement
+{
+    class DBLinqModel
+    {
+        public List<object> fields = new List<object>();
+        public List<bool> isInts = new List<bool>();
+
+        public void addField(object field, bool isInt)
+        {
+            fields.Add(field);
+            isInts.Add(isInt);
+        }
+    }
+
+
+    class DBLinq
+    {
+        private string connectionStr;
+        private DataSet dataSet = new DataSet();
+        private DataTable dataTable = new DataTable();
+
+        private string SEPRATOR = " , ";
+        private string COTATION = "'";
+
+        /**constants for queries**/
+        private const string VARCHAR = " VARCHAR ";
+        private const string INTEGER = " INT ";
+        private const string FLOAT = " FLOAT ";
+        private const string BOOLEAN = " BOOLEAN ";
+
+        private NpgsqlConnection connection;
+
+        public DBLinq()
+        {
+        }
+
+        public void initializeConnection(string conn)
+        {
+            // Making connection with Npgsql provider
+            connection = new NpgsqlConnection(conn);
+        }
+
+        /// <summary>
+        /// connect to DB based on connection string that have given!
+        /// </summary>
+        public void connectToDB()
+        {
+            connection.Open();
+        }
+
+        /// <summary>
+        /// closing the connection!
+        /// </summary>
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+
+
+        /// <summary>
+        /// return the whole of desired table! 
+        /// type => DataSet
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public DataSet getDataSet(string tableName)
+        {
+            string sql = "SELECT * FROM " + tableName;
+            // data adapter making request from our connection
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
+            // i always reset DataSet before i do
+            // something with it.... i don't know why :-)
+            dataSet.Reset();
+            // filling DataSet with result from NpgsqlDataAdapter
+            adapter.Fill(dataSet);
+
+            return dataSet;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public DataSet getDataSet(string tableName, string condition)
+        {
+            string sql = "SELECT * FROM " + tableName + " WHERE " + condition;
+            // data adapter making request from our connection
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
+            // i always reset DataSet before i do
+            // something with it.... i don't know why :-)
+            dataSet.Reset();
+            // filling DataSet with result from NpgsqlDataAdapter
+            Console.WriteLine("SQLLL : " + sql);
+            adapter.Fill(dataSet);
+
+
+            return dataSet;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public DataSet delete(string tableName, string condition)
+        {
+            string sql = "DELETE FROM " + tableName + " WHERE " + condition;
+            // data adapter making request from our connection
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
+            // i always reset DataSet before i do
+            // something with it.... i don't know why :-)
+            dataSet.Reset();
+            // filling DataSet with result from NpgsqlDataAdapter
+            adapter.Fill(dataSet);
+
+            return dataSet;
+        }
+
+
+        /// <summary>
+        /// inserting the new data!
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="values"></param>
+        public void insert(string tableName, DBLinqModel model)
+        {
+
+            /* we make query string here */
+            object[] fields = model.fields.ToArray();
+            bool[] isInts = model.isInts.ToArray();
+
+            string sql = "INSERT INTO " + tableName + " VALUES ( ";
+            //getting type of field and adding to sql string query !
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (!isInts[i])
+                {
+                    string sqlAppend = COTATION + fields[i] + COTATION + SEPRATOR;
+                    sql = sql + sqlAppend;
+                }
+                else
+                {
+                    string sqlAppend = fields[i] + SEPRATOR;
+                    sql = sql + sqlAppend;
+                }
+            }
+            // removing the last charcter of sql that is ','
+            sql = sql.Remove(sql.Length - 1);
+            sql = sql.Remove(sql.Length - 1);
+            sql = sql + ")";
+            /* end of making sql query string! */
+
+            Console.WriteLine("sql = \n" + sql);
+
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
+            // i always reset DataSet before i do
+            // something with it.... i don't know why :-)
+            dataSet.Reset();
+            // filling DataSet with result from NpgsqlDataAdapter
+            adapter.Fill(dataSet);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public DataSet update(string tableName, string[] attributes, string[] values, string condition)
+        {
+            string sql = "UPDATE " + tableName + " SET ";
+            for (int i = 0; i < attributes.Length; i++)
+            {
+                sql = sql + attributes[i] + " = " + values[i] + " , ";
+            }
+            sql = sql.Remove(sql.Length - 1);
+            sql = sql.Remove(sql.Length - 1);
+
+            sql = sql + "where " + condition;
+
+            // data adapter making request from our connection
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
+            // i always reset DataSet before i do
+            // something with it.... i don't know why :-)
+            dataSet.Reset();
+            // filling DataSet with result from NpgsqlDataAdapter
+            adapter.Fill(dataSet);
+
+            return dataSet;
+        }
+    }
+}
--- UsersManagement/Form1.Designer.cs
+++ UsersManagement/Form1.Designer.cs
@@ -39,61 +39,61 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(74, 33);
+            this.label1.Font = new System.Drawing.Font("B Traffic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(30, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 38);
+            this.label1.Size = new System.Drawing.Size(197, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Login";
+            this.label1.Text = "ورود به سیستم مدیریت";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Font = new System.Drawing.Font("B Compset", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(162, 100);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.Size = new System.Drawing.Size(79, 34);
             this.label2.TabIndex = 1;
-            this.label2.Text = "username";
+            this.label2.Text = "نام کاربری";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 154);
+            this.label3.Font = new System.Drawing.Font("B Compset", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(172, 141);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.Size = new System.Drawing.Size(69, 34);
             this.label3.TabIndex = 2;
-            this.label3.Text = "password";
+            this.label3.Text = "رمز عبور";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Location = new System.Drawing.Point(23, 206);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(222, 35);
+            this.button1.Size = new System.Drawing.Size(218, 35);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Login";
+            this.button1.Text = "ورود";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(114, 113);
+            this.textBox1.Location = new System.Drawing.Point(23, 107);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 154);
+            this.textBox2.Location = new System.Drawing.Point(23, 148);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
             this.textBox2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(246, 252);
+            this.ClientSize = new System.Drawing.Size(265, 272);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
